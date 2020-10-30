6af31b3e3743c391d3cedb96046565aa04552e1b
see-bet-code <brittani.elan.taylor@gmail.com>
2020-10-30 09:16:10 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskDesc = document.querySelector('input')
+  const form = document.querySelector('form')
+  const taskList = document.querySelector('ul')
+
+  form.addEventListener("submit", function(e) {
+    e.preventDefault()
+    function addTask() {
+      const li = document.createElement('li')
+      li.textContent = " " + taskDesc.value + " "
+      taskList.append(li)
+
+      function addButton() {
+        const button = document.createElement('button')
+        function removeLi() {
+          li.remove()
+        }
+        button.onclick = removeLi
+        button.textContent = 'x'
+        li.append(button)
+      }
+
+      addButton()
+    }
+
+    addTask()
+  })
+  
 });
